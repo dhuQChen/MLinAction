5c78a3d70ecffa0fd75b21af3aac7782072e42ea
dhuQChen <2322740596@qq.com>
2018-10-17 19:40:00 +0800

    Ch05

--- Ch05/logRegress.py
+++ Ch05/logRegress.py
@@ -46,7 +46,7 @@ def plotBestFit(weights):
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = np.arange(-3.0, 3.0, 0.1)
-    y = (- weights[0, 0] - weights[1, 0] * x) / weights[2, 0]
+    y = (- weights[0] - weights[1] * x) / weights[2]
     # y = (- weights[0][0] - weights[1][0] * x) / weights[2][0]    False
     ax.plot(x, y, c="blue")
     plt.xlabel('X1')
@@ -54,8 +54,90 @@ def plotBestFit(weights):
     plt.show()
 
 
-dataArr, labelMat = loadDataSet()
-weights = gradAscent(dataArr, labelMat)
-plotBestFit(weights)
+def stocGradAscent0(dataMatrix, classLabels):
+    m, n = np.shape(dataMatrix)
+    alpha = 0.01
+    weights = np.ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=200):
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(np.random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+
+# dataArr, labelMat = loadDataSet()
+# # Gradient Ascent
+# weights = gradAscent(dataArr, labelMat)
+# plotBestFit(weights.getA())
+
+# Stochastic Gradient Ascent
+# weights = stocGradAscent0(np.array(dataArr), labelMat)
+# plotBestFit(weights)
+
+# Advanced Stochastic Gradient Ascent
+# weights = stocGradAscent1(np.array(dataArr), labelMat)
+# plotBestFit(weights)
+
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 500)
+    print(trainWeights)
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate of this test is: %f" % errorRate)
+    return errorRate
+
+
+def multiTest():
+    numTests = 10; errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
+
+multiTest()
+
+
+
+
 
 
